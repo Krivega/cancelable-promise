ede7b0e8f4496d11128b05050a249b8b166259ee
Krivega Dmitriy <>
2023-10-21 12:20:13 +0300

    feat: export types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-export { default as cancelablePromise } from './cancelablePromise';
-export { isCanceledError, createErrorCanceled } from './error';
 export { default as CancelableRequest } from './CancelableRequest';
+export type { ITargetRequest } from './CancelableRequest';
+export { default as cancelablePromise } from './cancelablePromise';
+export type { ICancelablePromise } from './cancelablePromise';
+export { createErrorCanceled, isCanceledError } from './error';
+export type { IErrorCanceled } from './error';
